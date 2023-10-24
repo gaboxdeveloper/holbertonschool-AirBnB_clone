956a84bd04e54427bbfc80a7def5b45b89f091d7
Gabriel <grossy.sjb@gmail.com>
2023-10-24 21:54:06 +0000

    console v2

--- console.py
+++ console.py
@@ -3,17 +3,22 @@
 import cmd
 
 
-class HBNBCommand(cmd.Cmd):
-    """class for the console"""
-    prompt = "(hbnb) "
-
-    def do_quit(self):
-        """Exit the program"""
-        return True
-
-    def do_EOF(self):
-        """Exit the program (Ctrl+D)"""
-        return True
-
 if __name__ == '__main__':
+    import cmd
+
+    class HBNBCommand(cmd.Cmd):
+        prompt = '(hbnb) '
+
+        def do_quit(self, arg):
+            """Exit the program"""
+            return True
+
+        def do_EOF(self, arg):
+            """Exit the program (Ctrl+D)"""
+            return True
+
+        def emptyline(self):
+            """Do nothing on an empty line"""
+            pass
+
     HBNBCommand().cmdloop()

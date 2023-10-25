a71a0454ee659736c8b3e51fea55a6359f463fdd
Gabriel <grossy.sjb@gmail.com>
2023-10-25 22:54:39 +0000

    console

--- console.py
+++ console.py
@@ -7,14 +7,14 @@ class HBNBCommand(cmd.Cmd):
     """class for the console"""
     prompt = "(hbnb) "
 
-    def do_quit(self):
+    def do_quit(self, arg):
         """Exit the program"""
         return True
 
     def do_help(self, arg: str) -> bool | None:
-        return super().do_help(arg)    
+        return super().do_help(arg)
 
-    def do_EOF(self):
+    def do_EOF(self, arg):
         """Exit the program (Ctrl+D)"""
         return True
 

8aafe4457d63010d73bdefb52e82610c0a4a31a2
Gabriel <grossy.sjb@gmail.com>
2023-10-26 17:12:48 +0000

    console task7 v1

--- console.py
+++ console.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """Console module"""
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """class for the console"""
     prompt = "(hbnb) "
-
+    __classes = {
+        'BaseModel': BaseModel,
+        }
+    
     def do_quit(self, arg):
         """Quit command to exit the program\n"""
         return True
@@ -17,6 +22,39 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
+    def do_create(self, arg):
+        if not arg:
+            print("** class name missing **")
+        elif arg not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            obj = self.__classes[arg]()
+            storage.new(obj)
+            storage.save()
+            print(obj.id)
+
+    def do_show(self, arg):
+        """show info of the instance"""
+        args = arg.split()
+        if len(args) < 1:
+            print("** class name missing **")
+            return
+
+        if args[0] not in self.__classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+
+    def do_all(self, arg):
+        if arg not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(f"")
 
 
 if __name__ == '__main__':
